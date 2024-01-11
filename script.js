e2a7cb6472041af09d7d6365637be7bc3d58c57f
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-01-11 23:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // write js code here if required
+function updateTimer() {
+      const currentDate = new Date();
+      const formattedDateTime = currentDate.toLocaleString();
+      document.getElementById('timer').innerText = formattedDateTime;
+    }
+
+    setInterval(updateTimer, 1000);
+
+    // Initial update
+    updateTimer();
